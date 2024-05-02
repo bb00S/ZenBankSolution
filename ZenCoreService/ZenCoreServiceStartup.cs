e2813c45a7cf4184fa928a6f4c2f178eefd900b7
bb00S <bokiborovcanin2000@gmail.com>
2024-05-02 18:53:47 +0200

    ZenBankSolution1:Microsoft.EntityFrameworkCore.SqlServer package installed, ZenCoreService:ZenCoreServiceStartup file created and implemented services configuration;

---
+++ ZenCoreService/ZenCoreServiceStartup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ZenCoreService.Data;
+using ZenCoreService.Services;
+using ZenCoreService.Services.TransactionServices;
+using ZenCoreService.Services.Interfaces;
+using ZenCoreService.Interfaces;
+
+namespace ZenCoreService
+{
+    public class ZenCoreServiceStartup
+    {
+        public ZenCoreServiceStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ITransactionService, TransactionService>();
+            services.AddScoped<IUserService, UserService>();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
