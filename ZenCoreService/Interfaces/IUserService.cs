c2fded6ac2fc0b0edf4bf1fba5c395db03a9d222
bb00S <bokiborovcanin2000@gmail.com>
2024-05-02 18:31:39 +0200

    ZenCoreService: Created IUserService interface & UserService service implementation;

---
+++ ZenCoreService/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ZenCoreService.Models;
+
+namespace ZenCoreService.Services.Interfaces
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int userId);
+        User CreateUser(User user);
+        User UpdateUser(int userId, User updatedUser);
+        void DeleteUser(int userId);
+    }
+}
+
+
---
+++ ZenCoreService/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using ZenCoreService.Data;
+using ZenCoreService.Models;
+using ZenCoreService.Services.Interfaces;
+
+namespace ZenCoreService.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _dbContext.Users.ToList();
+        }
+
+        public User GetUserById(int userId)
+        {
+            return _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+        }
+
+        public User CreateUser(User user)
+        {
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return user;
+        }
+
+        public User UpdateUser(int userId, User updatedUser)
+        {
+            var existingUser = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (existingUser != null)
+            {
+                existingUser.Name = updatedUser.Name;
+                existingUser.Email = updatedUser.Email; 
+
+                _dbContext.SaveChanges();
+            }
+
+            return existingUser;
+        }
+
+        public void DeleteUser(int userId)
+        {
+            var userToDelete = _dbContext.Users.FirstOrDefault(u => u.Id == userId);
+            if (userToDelete != null)
+            {
+                _dbContext.Users.Remove(userToDelete);
+                _dbContext.SaveChanges();
+            }
+        }
+
+    }
+}
+
