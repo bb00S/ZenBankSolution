d695557beab367f3ed498d0f4f27a5df00037819
bb00S <bokiborovcanin2000@gmail.com>
2024-05-02 02:33:37 +0200

    ZenBankSolution:Microsoft.EntityFrameworkCore package installed; ZenCoreService: AppDbContext created;

---
+++ ZenCoreService/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using ZenCoreService.Models; 
+
+namespace ZenCoreService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
