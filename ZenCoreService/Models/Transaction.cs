a329763f82a7f72816c37cb9a70807c1454b1b27
bb00S <bokiborovcanin2000@gmail.com>
2024-05-02 02:02:26 +0200

    ZenCoreService: Created Models Transaction.cs & User.cs;

---
+++ ZenCoreService/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace ZenCoreService.Models
+{
+    public class Transaction
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
