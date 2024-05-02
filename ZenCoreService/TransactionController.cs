45a352ce1ca41cd8b473d50b5554943cf3d845d8
bb00S <bokiborovcanin2000@gmail.com>
2024-05-02 18:59:07 +0200

    ZenCoreService: API controller TransactionController developed;

---
+++ ZenCoreService/TransactionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Transactions;
+using ZenCoreService.Interfaces;
+
+namespace ZenCoreService.Controllers
+{
+    [ApiController]
+    [Route("api/transactions")]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionsController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Transaction>> GetTransactions()
+        {
+            var transactions = _transactionService.GetAllTransactions();
+            return Ok(transactions);
+        }
+
+    }
+}
