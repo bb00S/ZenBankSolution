868ed61578d61a6692a1fa844089e7e64d5313db
bb00S <bokiborovcanin2000@gmail.com>
2024-05-04 14:04:26 +0200

    ZenReportingService: Created Models Client.cs & ReportData.cs;

---
+++ ZenReportingService/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace ZenReportingService.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ ZenReportingService/Models/ReportData.cs
@@ -0,0 +1,18 @@
+﻿namespace ZenReportingService.Models
+{
+    public class ReportData
+    {
+        public DateTime CurrentDate { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public decimal TotalSum { get; set; }
+
+    }
+
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+
+    }
+}
