abd0daa54852d6ab21794f0fa00a2a10c48e3844
bb00S <bokiborovcanin2000@gmail.com>
2024-05-04 15:09:18 +0200

    ZenReportingService:ZenReportingServiceStartup file created and implemented services configuration;

---
+++ ZenReportingService/ZenReportingServiceStartup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ZenReportingService.Data;
+using ZenReportingService.Services;
+
+
+namespace ZenReportingService
+{
+    public class ZenReportingServiceStartup
+    {
+        public ZenReportingServiceStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddScoped<IEmailService, EmailService>();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
